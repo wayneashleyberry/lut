d9268c419c6cc06e091b6186b8280c2d58f34570
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2019-03-27 17:30:40 +0000

    add intensity

--- pkg/lut/lut.go
+++ pkg/lut/lut.go
@@ -15,7 +15,7 @@ func (c col32) RGBA() (uint32, uint32, uint32, uint32) {
 	return c.R, c.G, c.B, c.A
 }
 
-// Apply implementation
+// Apply colour transformations to an image from the provided lookup table
 func Apply(src, effect image.Image, intensity float64) (image.Image, error) {
 	if intensity < 0 || intensity > 1 {
 		return src, errors.New("intensity must be between 0 and 1")
@@ -28,29 +28,30 @@ func Apply(src, effect image.Image, intensity float64) (image.Image, error) {
 		image.Point{bounds.Max.X, bounds.Max.Y},
 	})
 
+	space := &image.NRGBA{}
+
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			c := src.At(x, y).(color.NRGBA)
+			// not all images use the same colour space, so ensure we convert
+			// them all to nrgba to be consistent with our output
+			px := src.At(x, y)
+			converted := space.ColorModel().Convert(px)
+			c := converted.(color.NRGBA)
 
+			// find the location of the pixel in our lookup table
 			lutx := int((c.B/4%8)*64 + c.R/4)
 			luty := int(math.Floor(float64(c.B/4)/8)*64 + float64(c.G/4))
 
 			lut := effect.At(lutx, luty).(color.RGBA)
 
+			// create our output colour, adjusted according to the intensity
 			o := color.NRGBA{}
-			o.R = lut.R
-			o.G = lut.G
-			o.B = lut.B
+			o.R = uint8(float64(c.R)*(1-intensity) + float64(lut.R)*intensity)
+			o.G = uint8(float64(c.G)*(1-intensity) + float64(lut.G)*intensity)
+			o.B = uint8(float64(c.B)*(1-intensity) + float64(lut.B)*intensity)
 			o.A = c.A
 
 			out.Set(x, y, o)
-
-			// if c.A != 0 {
-			// 	fmt.Printf(
-			// 		"x: %d y: %d col: %+v lut x: %d lut y: %d out: %+v\n",
-			// 		x, y, c, lutx, luty, o,
-			// 	)
-			// }
 		}
 	}
 
