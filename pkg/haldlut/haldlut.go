3539767ebcdb736ed4eda5e584534abc4811ae8d
wayneashleyberry <wayneashleyberry@gmail.com>
2019-09-02 15:17:24 +0100

    remove dependencies

--- pkg/haldlut/haldlut.go
+++ pkg/haldlut/haldlut.go
@@ -3,9 +3,9 @@ package haldlut
 import (
 	"errors"
 	"image"
+	"image/color"
 	"math"
 
-	"github.com/lucasb-eyer/go-colorful"
 	"github.com/overhq/lut/pkg/colorcube"
 	"github.com/overhq/lut/pkg/parallel"
 )
@@ -38,20 +38,30 @@ func Apply(src, effect image.Image, intensity float64) (image.Image, error) {
 		image.Point{bounds.Max.X, bounds.Max.Y},
 	})
 
+	space := &image.NRGBA{}
+	model := space.ColorModel()
+
 	width, height := bounds.Dx(), bounds.Dy()
 	parallel.Line(height, func(start, end int) {
 		for y := start; y < end; y++ {
 			for x := 0; x < width; x++ {
+				// not all images use the same colour space, so ensure we convert
+				// them all to nrgba to be consistent with our output
 				px := src.At(x, y)
+				c := model.Convert(px).(color.NRGBA)
 
-				c, _ := colorful.MakeColor(px)
-				r, g, b := c.RGB255()
+				lutx := (c.G/4%8)*64 + (c.R / 4)
+				luty := math.Floor(float64(c.G/4)/8) + float64(c.B/4)*8
 
-				lutx := (g/4%8)*64 + (r / 4)
-				luty := math.Floor(float64(g/4)/8) + float64(b/4)*8
+				l := effect.At(int(lutx), int(luty))
+				lut := model.Convert(l).(color.NRGBA)
 
-				at := effect.At(int(lutx), int(luty))
-				o, _ := colorful.MakeColor(at)
+				// create our output colour, adjusted according to the intensity
+				o := color.NRGBA{}
+				o.R = uint8(float64(c.R)*(1-intensity) + float64(lut.R)*intensity)
+				o.G = uint8(float64(c.G)*(1-intensity) + float64(lut.G)*intensity)
+				o.B = uint8(float64(c.B)*(1-intensity) + float64(lut.B)*intensity)
+				o.A = c.A
 
 				out.Set(x, y, o)
 			}
